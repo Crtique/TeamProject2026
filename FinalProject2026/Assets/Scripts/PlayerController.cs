e9226fae45d8e563113730810dc055b804b27e78
Crtique <robinsoncar345@gmail.com>
2026-04-02 14:44:07 -0400

    Movement

---
+++ FinalProject2026/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+/*Script was written by CJ Robinson. handle player movement using ridgidbody*/
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // --- Declare Variables ---
+    [Header("Player Movement")]
+    public float speed = 10f;
+
+    // --- Declare Components ---
+    private Rigidbody rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-Vector3.right * speed * 10f, ForceMode.Force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed * 10f, ForceMode.Force);
+        }
+    }
+}
