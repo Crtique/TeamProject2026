92e1b354845c5a8a10183fa813eb75f66a1530fd
nlash-fitch <nlash@student.fitchburgstate.edu>
2026-04-06 21:43:03 -0400

    Update CameraController

--- FinalProject2026/Assets/Scripts/CameraController.cs
+++ FinalProject2026/Assets/Scripts/CameraController.cs
@@ -6,8 +6,7 @@ public class CameraController : MonoBehaviour
     public Rigidbody playerRB;
     public Vector3 offset = new Vector3(0,2,-10f);
     public Vector3 velocity = new Vector3(0,0,0);
-    public float speed = 5f;
-    public float speed2 = .2f;
+    public float speed = .2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,9 +15,10 @@ public class CameraController : MonoBehaviour
 
     void LateUpdate()
     {
-        //velocity = new Vector3(playerRB.linearVelocity.x, playerRB.linearVelocity.y, playerRB.linearVelocity.z);
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(playerRB.transform.position.x, playerRB.transform.position.y + offset.y, offset.z), ref velocity, speed2);
-        //transform.position = Vector3.MoveTowards(transform.position, new Vector3(playerRB.transform.position.x, playerRB.transform.position.y + offset.y, offset.z), speed);
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(playerRB.transform.position.x, playerRB.transform.position.y + offset.y, offset.z), ref velocity, speed);
+        if(velocity.magnitude < 0.0001) {
+            velocity = new Vector3(0,0,0);
+        } 
     }
 
 
