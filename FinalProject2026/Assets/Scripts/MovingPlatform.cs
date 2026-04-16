0873199065cdf24948f3b7425155e7694992fdd7
Autumn <nlash@student.fitchburgstate.edu>
2026-04-16 16:25:35 -0400

    localPosition

--- FinalProject2026/Assets/Scripts/MovingPlatform.cs
+++ FinalProject2026/Assets/Scripts/MovingPlatform.cs
@@ -4,21 +4,22 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    public Vector3 startPOS;
     public Vector3[] waypoints;
     public float speed = 1f;
     public int pointer = 0;
+    public bool startMoveWhenPlayer = false; //set to true to have platform start moving only after the player steps on it 
     void Start()
     {
-        //startPOS = transform.position;
-        transform.position = waypoints[0];
+        transform.localPosition = waypoints[0];
     }
 
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, waypoints[pointer], speed * Time.deltaTime);
-        if (transform.position == waypoints[pointer]) {
-            updateWaypoint();
+        if (!startMoveWhenPlayer) {
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, waypoints[pointer], speed * Time.deltaTime);
+            if (transform.localPosition == waypoints[pointer]) {
+                updateWaypoint();
+            }
         }
     }
 
@@ -32,6 +33,13 @@ public class MovingPlatform : MonoBehaviour
         pointer++;
         if(pointer >= waypoints.Length) {
             pointer = 0;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "Player") {
+            startMoveWhenPlayer = false;
         }
     }
 }
