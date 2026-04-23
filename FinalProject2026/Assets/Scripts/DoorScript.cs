df0e49bc8909d533fea439da19edc15796e1cd15
Autumn <nlash@student.fitchburgstate.edu>
2026-04-23 13:42:40 -0400

    doors (mostly) working

--- FinalProject2026/Assets/Scripts/DoorScript.cs
+++ FinalProject2026/Assets/Scripts/DoorScript.cs
@@ -2,15 +2,29 @@ using UnityEngine;
 
 public class DoorScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool doorActive = false;
+    public GameObject exit;
+    private GameObject player;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(doorActive && Input.GetKeyDown(KeyCode.E)) {
+            player.transform.position = exit.transform.position;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            doorActive = true;
+            player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player") {
+            doorActive = false;
+        }
     }
 }
