e6ab2a11bbbe02edb468375c1fb306864b74f5fd
Crtique <robinsoncar345@gmail.com>
2026-04-29 23:41:41 -0400

    Loading screen

---
+++ FinalProject2026/Assets/Scripts/UI Scripts/LoadingScene.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+
+public class LoadingScene : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Image loadingBarFill;
+
+    public void LoadScene(int sceneID)
+    {
+        StartCoroutine(LoadSceneAsync(sceneID));
+    }
+
+    IEnumerator LoadSceneAsync(int sceneID)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
+
+        loadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+
+            loadingBarFill.fillAmount = progressValue;
+
+            yield return null;
+        }
+    }
+}
